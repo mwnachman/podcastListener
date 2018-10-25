a7b3a32e366f34f4b6e35a21d3d1ec6ac3f35475
Meredith Nachman <mwnachman@gmail.com>
2018-10-25 19:30:40 -0400

    add dependencies

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Container from './Container.jsx'
+
+const App = () => {
+  return (
+    <Container/>
+  )
+}
+
+export default App
---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className="blue">hasdfh</div>
+  )
+}
+
+export default Container
